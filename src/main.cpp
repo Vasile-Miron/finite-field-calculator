399e25b630a3c2f758d14db5c9932b4b6a4f2025
nemrod153 <vasilemiron610@gmail.com>
2025-12-03 16:31:29 +0100

    Restructured main.cpp input. Added termsInput, which processes a and b with error handling and builds FieldElement a and b.

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,23 @@
 #include <sstream>
 #include <finiteFieldElement.hpp>
 
-int main() {
+using FE = ff::runtime::FieldElement;
 
-    //TODO: implement calculator program using the finiteFieldElement header
-    //TODO: consider and create advanced math header for finite field operations
-
-    std::ios::sync_with_stdio(false);
-    uint64_t mod;
+uint64_t modInput () {
     std::ostringstream err;
+    uint64_t modulus = 2;
 
-    //input validation loop
     while (true) {
         std::cout << "Enter prime modulus p: ";
 
-        if (!(std::cin >> mod)) {
+        if (!(std::cin >> modulus)) {
             err << "Invalid input: Expected a positive integer.";
             std::cin.clear();
             std::cin.ignore(9999, '\n');
         } else {
             try {
-                ff::runtime::FieldElement::setModulus(mod);
+                ff::runtime::FieldElement::setModulus(modulus);
+                std::cout << "Initialized finite field GF(" << modulus << ") successfully!" << std::endl;
                 break;
             } catch (const std::invalid_argument& e) {
                 err << "Invalid modulus: " << e.what();
@@ -34,6 +31,49 @@ int main() {
             err.clear();
         }
     }
+    return modulus;
+}
+
+void termsInput (FE& a, FE& b, const uint64_t& modulus) {
+    uint64_t aRaw;
+    uint64_t bRaw;
+
+    std::cout << std::endl;
+    while (true) {
+        std::cout << "Enter term a in GF(" << modulus << "): ";
+
+        if (!(std::cin >> aRaw)) {
+            std::cout << "Invalid input: Expected an integer.\n\n" << std::flush;
+            std::cin.clear();
+            std::cin.ignore(9999, '\n');
+        } else break;
+    }
+    a = FE(aRaw);
+    std::cout << "Initialized term GF(" << modulus << ") a = " << a << " successfully!" << std::endl;
+
+    while (true) {
+        std::cout << "Enter term b in GF(" << modulus << "): ";
+
+        if (!(std::cin >> bRaw)) {
+            std::cout << "Invalid input: Expected an integer.\n\n" << std::flush;
+            std::cin.clear();
+            std::cin.ignore(9999, '\n');
+        } else break;
+    }
+    b = FE(bRaw);
+    std::cout << "Initialized term GF(" << modulus << ") b = " << b << " successfully!" << std::endl;
+}
+
+int main() {
+
+    //TODO: implement calculator program using the finiteFieldElement header
+    //TODO: consider and create advanced math header for finite field operations
+
+    std::ios::sync_with_stdio(false);
+    const uint64_t modulus = modInput();
+    FE a, b;
+
+    termsInput(a, b, modulus);
 
 
     return 0;
